b53e80b1272e89e17eae80187a676b575751c392
Ohshima Yusuke (s-giken.com) <ooshima@s-giken.com>
2024-06-27 08:05:40 +0000

    ビルド用のバッチファイルを追加

---
+++ src/pictures/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+PROGRAMNAME="PICTURES.COB"
+BINDIR="${SCRIPTDIR}"
+COPYLIBDIR="${SCRIPTDIR}/../../copylib"
+DIRNAME=$(basename "${SCRIPTDIR}")
+BINNAME=$(basename -s .COB $PROGRAMNAME)
+
+cobc -x -o "${BINDIR}/${BINNAME}" -I"${COPYLIBDIR}" "${PROGRAMNAME}"
+
+export OTF=result.txt
+${BINDIR}/${BINNAME}

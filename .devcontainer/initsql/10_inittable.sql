00f26bf10426578ac3f6bed31a9cc9c60ce8937a
Ohshima Yusuke (s-giken.com) <ooshima@s-giken.com>
2024-07-09 01:47:13 +0000

    実用的なデータに変更

--- .devcontainer/initsql/10_inittable.sql
+++ .devcontainer/initsql/10_inittable.sql
@@ -1,12 +1,24 @@
-create table if not exists userinfo (
-    userid      char(5) primary key,
-    username    char(32) not null,
-    userage     char(5) not null
+CREATE TABLE SalesData (
+    DATA_KBN CHAR(1),
+    JUCHU_NO NUMERIC(4),
+    JUCHU_YY NUMERIC(2),
+    JUCHU_MM NUMERIC(2),
+    JUCHU_DD NUMERIC(2),
+    TOKU_COD CHAR(4),
+    SHOHIN_NO CHAR(5),
+    SURYO NUMERIC(5)
 );
 
 
-insert into userinfo(userid, username, userage) values 
-('0001', 'Giken Taro', '0042'),
-('0002', 'Giken Hanako', '0032'),
-('0003', 'Giken Saburo', '0022')
+INSERT INTO SalesData (DATA_KBN, JUCHU_NO, JUCHU_YY, JUCHU_MM, JUCHU_DD, TOKU_COD, SHOHIN_NO, SURYO) VALUES
+('0', 1234, 23, 7, 9, '1001', 'A1234', 500),
+('0', 5678, 23, 7, 8, '1002', 'B2345', 1000),
+('9', 9101, 23, 7, 7, '1003', 'C3456', 1500),
+('0', 1123, 23, 7, 6, '1004', 'D4567', 2000),
+('9', 4567, 23, 7, 5, '1005', 'E5678', 300),
+('0', 8910, 23, 7, 4, '1006', 'F6789', 700),
+('0', 2345, 23, 7, 3, '1007', 'G7890', 1200),
+('9', 6789, 23, 7, 2, '1008', 'H8901', 400),
+('0', 1011, 23, 7, 1, '1009', 'I9012', 800),
+('0', 3456, 23, 7, 10, '1010', 'J0123', 600)
 ;
---
+++ sample/fetchdb/SALESDATA.cpy
@@ -0,0 +1,26 @@
+      * アクセスするテーブルの情報(任意、なくてもよい)
+      * DB2では、データ型のチェックを行っていて、異なると警告を出す。
+       EXEC SQL 
+           DECLARE TABLE SalesData (
+               DATA_KBN CHAR(1),
+               JUCHU_NO NUMERIC(4),
+               JUCHU_YY NUMERIC(2),
+               JUCHU_MM NUMERIC(2),
+               JUCHU_DD NUMERIC(2),
+               TOKU_COD CHAR(4),
+               SHOHIN_NO CHAR(5),
+               SURYO NUMERIC(5)
+           )
+       END-EXEC.
+      
+      * DBの情報を格納するための集団項目
+       01 SALESDATA.
+         03  SALES-DATA_KBN   PIC X(1).
+         03  SALES-JUCHU_NO   PIC 9(4).
+         03  SALES-JUCHU_YY   PIC 9(2).
+         03  SALES-JUCHU_MM   PIC 9(2).
+         03  SALES-JUCHU_DD   PIC 9(2).
+         03  SALES-TOKU_COD   PIC X(4).
+         03  SALES-SHOHIN_NO  PIC X(5).
+         03  SALES-SURYO      PIC 9(5).
+           
